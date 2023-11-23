95e04e2e42395a5e449e6ead5b2bc060e6051853
joseluismaza <130082810+joseluismaza@users.noreply.github.com>
2023-11-23 22:24:00 +0100

    Add files via upload

---
+++ EjerciciosJavaScript/ejercicio1.js
@@ -0,0 +1,17 @@
+// Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
+// Crea una variable llamada x, asigna el valor 50 a ella
+// Crea una variable llamada "h" con el valor 5  y otra "y" con el valor 10
+// Crea una otra variable "z" y asignale el valor de "h" + "y"
+
+let myFavoriteHero = "Hulk";
+console.log(myFavoriteHero)
+
+let x = 50;
+console.log(x)
+
+let h = 5, y = 10;
+console.log(h, y)
+
+let z = h + y;
+
+console.log(z)
---
+++ EjerciciosJavaScript/ejercicio2.js
@@ -0,0 +1,22 @@
+// Dado el siguiente objeto, cambia el valor de la propiedad age a 25.
+const character = { name: 'Jack Sparrow', age: 10 };
+
+/* Declara 3 variables con los nombres y valores siguientes
+
+firstnName = 'Jon';
+lastName = 'Snow';
+age = '24';
+
+Muestralos por consola de esta forma: 'Soy Jon Snow, tengo 24 años y me gustan los lobos.'
+*/
+
+// Dado el siguiente código, imprime con uin console.log la suma del precio de ambos juguetes
+const toy1 = { name: 'Buss myYear', price: 19 };
+const toy2 = { name: 'Rallo mcKing', price: 29 };
+
+
+/* Dado el siguiente código, actualiza el valor de la variable globalBasePrice a 25000 y actualiza la propiedad
+finalPrice a todos los coches con el valor de su propiedad basePrice más el valor de la variable globalBasePrice.*/
+
+let globalBasePrice = 10000;
+const car1 = { name: 'BMW m&m', basePrice: 50000, finalPrice: 60000 };
